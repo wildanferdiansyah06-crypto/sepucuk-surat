c9a967f8d157419aa7f5a963f0df5e7d18525b65
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-02-25 01:52:20 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@/react-app/index.css";
-import App from "@/react-app/App.tsx";
+import "./index.css";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
