8b7901cc3d4682a5d1e6f59e9146f8a60d99a5e4
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-02-25 01:06:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { mochaPlugins } from "@getmocha/vite-plugins";
 
 export default defineConfig({
   plugins: [
-    ...mochaPlugins(process.env as any),
     react(),
   ],
   server: {
