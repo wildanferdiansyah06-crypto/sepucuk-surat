46e09a8a80eae75c19189bd1db1e95c525ca8e7f
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-06 01:22:25 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import HomePage from "./Home";
 import Rak from "./Rak";
+import Reader from "./Reader";
+import Kirim from "./Kirim";
 
 export default function App() {
 return (
 <Router>
 <Routes>
 
-    {/* Halaman utama */}
+    {/* Homepage */}
     <Route path="/" element={<HomePage />} />
 
-    {/* Halaman rak buku */}
+    {/* Rak Buku */}
     <Route path="/rak" element={<Rak />} />
+
+    {/* Reader Mode */}
+    <Route path="/buku/:id" element={<Reader />} />
+
+    {/* Kirim Karya */}
+    <Route path="/kirim" element={<Kirim />} />
 
   </Routes>
 </Router>
