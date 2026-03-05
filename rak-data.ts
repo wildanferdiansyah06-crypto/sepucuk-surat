24be619ec404709625fee13241d3f9502e92e792
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-06 00:57:47 +0800

    Add RAK_BUKU array with book details

---
+++ rak-data.ts
@@ -0,0 +1,30 @@
+export const RAK_BUKU = [
+{
+id: 1,
+judul: "Seni Menyeduh Kehidupan",
+penulis: "Wildan Ferdiansyah",
+cover: "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?q=80&w=800",
+link: "https://drive.google.com/file/d/17Zd1FKFK4X_vmKhITFU5lXihOmMEkezI/view"
+},
+{
+id: 2,
+judul: "Di Balik Bar",
+penulis: "Wildan Ferdiansyah",
+cover: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=800",
+link: "https://drive.google.com/file/d/1N1zwGLqkbVQOzFV_fpRXJxQdawbgZGAl/view"
+},
+{
+id: 3,
+judul: "Di Atas Cangkir Yang Sama",
+penulis: "Wildan Ferdiansyah",
+cover: "https://images.unsplash.com/photo-1498804103079-a6351b050096?q=80&w=800",
+link: "https://drive.google.com/file/d/1cqRI8rfb7_0MIUXLekZJtV0xTFKXr-CD/view"
+},
+{
+id: 4,
+judul: "Kami Menulis Pelan",
+penulis: "Wildan Ferdiansyah",
+cover: "https://images.unsplash.com/photo-1455390582262-044cdead277a?q=80&w=800",
+link: "https://drive.google.com/file/d/1Mc6pOQ5z2xSn8Wmhf65kdgTrv5T5EzPm/view"
+}
+];
