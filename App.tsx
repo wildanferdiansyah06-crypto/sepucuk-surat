2b1f115a19035d7b60710419d7e86886fc99e04d
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-05 23:13:56 +0800

    Add route for Rak component in App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Home";
+import Rak from "./Rak";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/rak" element={<Rak />} />
       </Routes>
     </Router>
   );
