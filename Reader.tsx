c369f05669a68e657515c3f696755ce3e49e1686
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-06 17:48:57 +0800

    Update Reader.tsx

--- Reader.tsx
+++ Reader.tsx
@@ -1,11 +1,37 @@
 import { useParams, Link } from "react-router-dom";
-import { BUKU } from "./buku-data";
+
+const BUKU = [
+{
+id: "1",
+judul: "Seni Menyeduh Kehidupan",
+penulis: "Wildan Ferdiansyah",
+file: "/buku/seni-menyeduh-kehidupan.pdf"
+},
+{
+id: "2",
+judul: "Di Balik Bar",
+penulis: "Wildan Ferdiansyah",
+file: "/buku/di-balik-bar.pdf"
+},
+{
+id: "3",
+judul: "Di Atas Cangkir Yang Sama",
+penulis: "Wildan Ferdiansyah",
+file: "/buku/di-atas-cangkir.pdf"
+},
+{
+id: "4",
+judul: "Kami Menulis Pelan",
+penulis: "Wildan Ferdiansyah",
+file: "/buku/kami-menulis-pelan.pdf"
+}
+];
 
 export default function Reader() {
 
 const { id } = useParams();
 
-const buku = BUKU.find((b) => String(b.id) === id);
+const buku = BUKU.find((b) => b.id === id);
 
 if (!buku) {
 return (
